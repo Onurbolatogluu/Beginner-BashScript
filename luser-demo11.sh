c92b7f31231c82bfa957742582213f6730e0227d
Onurbolatogluu <bolatogluu@gmail.com>
2022-01-18 14:54:38 +0000

    Parsing Command Line Options with getopts

---
+++ luser-demo11.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+# This script generates a random password.
+# This user can set the password lenght with -l and add a special character with -s.
+# Verbose mode can be enabled with -v.
+
+# Set a default password lenght.
+LENGTH=48
+
+usage() {
+	echo "Usage: ${0} [-vs] [-l LENGTH]" >&2
+        echo 'Generate a random password.'	
+	echo ' -l LENGHT Specify the password length.'
+	echo ' -s Append a special  character to the password.'
+	echo ' -v Increase verbosity'
+	exit 1
+}
+
+
+log() {
+	local MESSAGE="${@}"
+	if [[ "${VERBOSE}" = 'true' ]]
+	then
+		echo "${MESSAGE}"
+	fi
+}
+
+while getopts vl:s OPTION
+do
+	case ${OPTION} in
+		v)
+			VERBOSE='true'
+			log 'Verbose mode on.'
+			;;
+		l)
+			LENGTH="${OPTARG}"
+			;;
+		s)
+			USE_SPECIAL_CHARACTER='true'
+			;;
+		?)
+			usage
+			;;
+	esac
+done
+
+log 'Generating a password.'
+
+PASSWORD=$(date +%s%N${RANDOM}${RANDOM} | sha256sum | head -c${LENGTH})
+
+# Append a special character if requested to do so.
+if [[ "${USE_SPECIAL_CHARACTER}" = 'true' ]]
+then
+	log 'Selecting a random special character.'
+	SPECIAL_CHARACTER=$(echo '!*=====////@+%&?=(-' | fold -w1 | shuf | head -c1)
+	PASSWORD="${PASSWORD}${SPECIAL_CHARACTER}"
+
+fi
+
+
+log 'Done.'
+log 'Here is the password:'
+
+# Display the password.
+echo "${PASSWORD}"
+
+exit 0
